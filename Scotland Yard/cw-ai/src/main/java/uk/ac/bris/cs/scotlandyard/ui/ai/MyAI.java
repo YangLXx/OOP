e3789311f5ebf2ef527198d121ca52ff98e487d5
Hatsune1 <15210065800@163.com>
2018-04-21 17:54:15 +0100

    4/21 AI

--- Scotland Yard/cw-ai/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAI.java
+++ Scotland Yard/cw-ai/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAI.java
@@ -3,6 +3,7 @@ package uk.ac.bris.cs.scotlandyard.ui.ai;
 import java.util.*;
 import java.util.function.Consumer;
 
+import uk.ac.bris.cs.gamekit.graph.Edge;
 import uk.ac.bris.cs.gamekit.graph.Graph;
 import uk.ac.bris.cs.gamekit.graph.Node;
 import uk.ac.bris.cs.scotlandyard.ai.ManagedAI;
@@ -19,23 +20,94 @@ public class MyAI implements PlayerFactory {
 	}
 
 	private static class MyPlayer implements Player{
-		private int Dijkstras (ScotlandYardView view, int destination, Colour targetDetective){
-			Graph<Integer, Transport> graph = view.getGraph();
-			HashMap<Node<Integer>, Integer> steps = new HashMap<>();
-			
-			return 0;
+
+		int moveDestination;
+
+		// Copy the number of remained tickets from ScotlandYardView to a new Map<Ticket, Integer>
+		// for counting the remaining tickets in searching of least steps
+		private void setTargetDetectiveTickets (ScotlandYardView view,
+								 Colour targetDetective,
+								 Map<Ticket, Integer> targetDetectiveTickets){
+			targetDetectiveTickets.put(Ticket.TAXI, view.getPlayerTickets(targetDetective, Ticket.TAXI).get());
+			targetDetectiveTickets.put(Ticket.BUS, view.getPlayerTickets(targetDetective, Ticket.BUS).get());
+			targetDetectiveTickets.put(Ticket.UNDERGROUND, view.getPlayerTickets(targetDetective, Ticket.UNDERGROUND).get());
+
 		}
 
+		// Start search from current node
+		public void startSearchingFrom (Node<Integer> currentNode,
+										Graph<Integer, Transport> graph,
+										Map<Ticket, Integer> targetDetectiveTickets,
+										HashMap<Node<Integer>, Integer> steps){
+			// Get the possible edges from currentNode
+			Collection<Edge<Integer, Transport>> possibleEdges = new HashSet<>(graph.getEdgesFrom(currentNode));
 
+			for (Edge<Integer, Transport> possibleEdge : possibleEdges) {
+				if (targetDetectiveTickets.get(Ticket.fromTransport(possibleEdge.data())) != 0 &&
+						(currentNode.value() + 1 < steps.get(possibleEdge.destination()) ||
+						steps.get(possibleEdge.destination()) == 0)){
+					// Create a new Map for tickets
+					Map<Ticket, Integer> targetDetectiveCURRENTTickets = targetDetectiveTickets;
+					// Set the value to n + 1
+					steps.replace(possibleEdge.destination(), currentNode.value() + 1);
+					// Decrease the consumed ticket
+					targetDetectiveCURRENTTickets.replace(Ticket.fromTransport(possibleEdge.data()),
+							targetDetectiveTickets.get(Ticket.fromTransport(possibleEdge.data())) - 1);
+					// Start searching from next node
+					startSearchingFrom(possibleEdge.destination(), graph, targetDetectiveCURRENTTickets, steps);
+				}
+			}
+		}
 
+		// Return the lease steps form the position of targetDetective to the destination
+		private int Dijkstras (ScotlandYardView view, int destination, Colour targetDetective){
+			// Create a new Map of Tickets to recode the remaining tickets of the targetDetective
+			Map<Ticket, Integer> targetDetectiveTickets = new HashMap<>();
+			// Create a Map for recording steps from targetDetective to each node
+			HashMap<Node<Integer>, Integer> steps = new HashMap<>();
+			setTargetDetectiveTickets(view, targetDetective, targetDetectiveTickets);
+			// Set steps all zero initially
+			for (Node<Integer> node : view.getGraph().getNodes()) steps.put(node, 0);
 
-		private Map<Move, Integer> leastSteps (ScotlandYardView view, Move move) {
- 			Map<Move, Integer> leastSteps = new HashMap<>();
- 			return leastSteps;
+			// Locate the location of targetDetective and start searching from there
+			for (Node<Integer> node: steps.keySet()) {
+				if (node.equals(new Node<>(view.getPlayerLocation(targetDetective).get()))){
+					startSearchingFrom(node, view.getGraph(), targetDetectiveTickets, steps);
+				}
+			}
+
+			return steps.get(new Node<>(destination));
+		}
+
+		private int totalLeastSteps (ScotlandYardView view, int moveDestination){
+			int totalLeastSteps = 0;
+			for (Colour player : view.getPlayers()){
+				totalLeastSteps = totalLeastSteps + Dijkstras(view, moveDestination, player);
+			}
+			return totalLeastSteps;
 		}
 
 		private int score (ScotlandYardView view, Move move){
-			int furtherEdgesScore;
+			// Create MoveVisitors and override visit to get final destination for different moves
+			MoveVisitor ticketMoveVisitor = new MoveVisitor() {
+				@Override
+				public void visit(TicketMove move) {
+					moveDestination = move.destination();
+				}
+			};
+			MoveVisitor doubleMoveVisitor = new MoveVisitor() {
+				@Override
+				public void visit(DoubleMove move) {
+					moveDestination = move.finalDestination();
+				}
+			};
+			if (move.getClass().equals(TicketMove.class)) move.visit(ticketMoveVisitor);
+			if (move.getClass().equals(DoubleMove.class)) move.visit(doubleMoveVisitor);
+
+			int totalLeastSteps = totalLeastSteps(view, moveDestination);
+			// TODO furtherValidmoves is much better than furtherEdges
+			int furtherEdges = view.getGraph().getEdgesFrom(new Node<Integer> (moveDestination)).size();
+			int FengShui;
 			boolean thisRound = view.getRounds().get(view.getCurrentRound());
 			boolean nextRound = view.getRounds().get(view.getCurrentRound() + 1);
 			//Give the formula of score
@@ -45,6 +117,7 @@ public class MyAI implements PlayerFactory {
 
 		private ArrayList<Move> pickBestMoves (Map<Move, Integer> moveScores){
 			ArrayList<Move> bestMoves = new ArrayList<>();
+			// TODO
 			return bestMoves;
 		}
 
@@ -63,6 +136,7 @@ public class MyAI implements PlayerFactory {
 			final Random random = new Random();
 			theBestMove = bestMoves.get(random.nextInt(bestMoves.size()));
 			// Plan B: Considering one more step or using another standard to choose theBestMove
+			// TODO
 			// callback
 			callback.accept(theBestMove);
 		}
